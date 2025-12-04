f69c48e9280b334f7601cc8d4bcda6e6c905f903
Kasper Radom <radom.kasper@gmail.com>
2025-12-04 01:39:00 +0100

    Powrot

--- main.py
+++ main.py
@@ -1,34 +1,17 @@
-from fastapi import FastAPI, Form, Request, status
-from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-import uvicorn
-
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
-    print('Request for index page received')
-    return templates.TemplateResponse('index.html', {"request": request})
+# Definiujemy model danych, który będzie wysyłany/odbierany w JSON
+class Message(BaseModel):
+    text: str
 
-@app.get('/favicon.ico')
-async def favicon():
-    file_name = 'favicon.ico'
-    file_path = './static/' + file_name
-    return FileResponse(path=file_path, headers={'mimetype': 'image/vnd.microsoft.icon'})
+@app.get("/")
+def read_root():
+    return {"message": "Hello, Azure!"}
 
-@app.post('/hello', response_class=HTMLResponse)
-async def hello(request: Request, name: str = Form(...)):
-    if name:
-        print('Request for hello page received with name=%s' % name)
-        return templates.TemplateResponse('hello.html', {"request": request, 'name':name})
-    else:
-        print('Request for hello page received with no name or blank name -- redirecting')
-        return RedirectResponse(request.url_for("index"), status_code=status.HTTP_302_FOUND)
-
-if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8000)
-
+@app.post("/echo")
+def echo_message(msg: Message):
+    # Endpoint przyjmuje JSON i zwraca ten sam JSON
+    return {"received": msg.text}
